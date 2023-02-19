0e2afbcbfd8b9acece5efb2210ebc5fac0b10935
Sailesh1993 <karki.saileshnep@gmail.com>
2023-02-19 12:56:02 +0545

    first commit

---
+++ war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Sailesh Karki on 4.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
